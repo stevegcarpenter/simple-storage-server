e4e4205bd51317b4093711d3de723c2b20c1f787
Steven Carpenter <stevegcarpenter@gmail.com>
2018-04-17 09:36:24 -0700

    Add some auth model tests

---
+++ __test__/unit/auth.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Auth = require('../../model/auth.js');
+
+describe('Auth Module', function () {
+  let mock = {username: 'steve', password: 'a;nmgoiwl;@#'};
+  let auth = new Auth();
+
+  describe('#Auth Username', function () {
+    it('Should have an _id property', () => {
+      expect(auth).toHaveProperty('_id');
+    });
+
+    it('Should have a username property', () => {
+      expect(new Auth(mock)).toHaveProperty('username');
+    });
+
+    it('Should have a password property', () => {
+      expect(new Auth(mock)).toHaveProperty('password');
+    });
+
+    it('should be an instance of an Object', () => {
+      expect(auth).toBeInstanceOf(Object);
+    });
+  });
+});
