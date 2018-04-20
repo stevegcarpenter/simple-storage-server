5255ce1ba67d4cfae39b0f53d26d0573a5b4fbe2
Steven Carpenter <stevegcarpenter@gmail.com>
2018-04-20 16:42:04 -0700

    No need to nest the model unit tests twice

--- __test__/unit/file.test.js
+++ __test__/unit/file.test.js
@@ -4,36 +4,34 @@ const File = require('../../model/file.js');
 
 describe('File Module', () => {
   let mock = {
-    name: 'fakefile',
+    filename: 'fakefile',
     userId: 'abc123',
     objectKey: '123543243546',
     fileURI: 'http://a-file-uri',
   };
   let file = new File();
 
-  describe('#File', () => {
-    it('Should have an _id property', () => {
-      expect(file).toHaveProperty('_id');
-    });
+  it('Should have an _id property', () => {
+    expect(file).toHaveProperty('_id');
+  });
 
-    it('Should have a name property', () => {
-      expect(new File(mock)).toHaveProperty('name');
-    });
+  it('Should have a filename property', () => {
+    expect(new File(mock)).toHaveProperty('filename');
+  });
 
-    it('Should have a userId property', () => {
-      expect(new File(mock)).toHaveProperty('userId');
-    });
+  it('Should have a userId property', () => {
+    expect(new File(mock)).toHaveProperty('userId');
+  });
 
-    it('Should have a objectKey property', () => {
-      expect(new File(mock)).toHaveProperty('objectKey');
-    });
+  it('Should have a objectKey property', () => {
+    expect(new File(mock)).toHaveProperty('objectKey');
+  });
 
-    it('Should have a fileURI property', () => {
-      expect(new File(mock)).toHaveProperty('fileURI');
-    });
+  it('Should have a fileURI property', () => {
+    expect(new File(mock)).toHaveProperty('fileURI');
+  });
 
-    it('should be an instance of an Object', () => {
-      expect(file).toBeInstanceOf(Object);
-    });
+  it('should be an instance of an Object', () => {
+    expect(file).toBeInstanceOf(Object);
   });
 });
